7c1e0c3cb94169534491ff2699d8ae06de1ddcdf
keb178 <3088186781@qq.com>
2026-04-14 20:05:15 +0800

    更新Python练习文件

---
+++ 2026/day_4.py
@@ -0,0 +1,39 @@
+#print("hello world")
+"""
+print("今天是正式打卡的第一天")
+message ="hello python world!"
+print(message)
+
+message="hello python Crash course world!"
+print(message)"""
+'''
+message="Hello python crash course reader!"
+print(mesage)#未定义
+'''
+#字符串
+'''name="ada lovelace"
+#print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name="ada"
+last_name="lovelace"
+full_name=f"{first_name} {last_name}"
+print(full_name)
+print(f"Hello,{full_name}!")
+message=f"Hello,{full_name.title()}!"
+print(message)
+
+print("python")
+print("\tpython")
+print("language:\t\npython\t\nc\t\njavaScript")
+'''
+#删除空白
+favorite_language = 'python '#favorite:最喜欢的   language:语言  favorite_language:偏好语言
+favorite_language
+'python '
+favorite_language.rstrip()#retirp:右侧去除，尾部剥离
+'python'
+favorite_language
+'python'
+
